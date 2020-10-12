cf779ff9b086ac0ec825e180e34c4462d39fb863
lordfoom <timothys@soliditech.com>
2020-10-12 20:30:15 +0200

    Implements user details service

--- src/main/kotlin/improbableotter/sideprojects/pufftime/user/User.kt
+++ src/main/kotlin/improbableotter/sideprojects/pufftime/user/User.kt
@@ -1,8 +1,12 @@
 package improbableotter.sideprojects.pufftime.user
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import javax.persistence.*
 
@@ -11,23 +15,26 @@ import javax.persistence.*
 class User(
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         var id: Long = -1,
-        @Column(nullable=false, unique = true)
+        @Column(nullable = false, unique = true)
         var username: String = "",
-        @Column(nullable=false)
+        @Column(nullable = false)
         var password: String = "",
         @Column(unique = true)
         var email: String = "",
         @Column
-        var role: String = "USER" ) {
+        var role: String = "USER") {
     override fun toString(): String {
         return "User id = $id, name = '$username', email = '$email', role = '$role'"
     }
 }
 
 @Service
-class UserDetailsServiceImpl(val userRepository: UserRepository): UserDetailsService {
+class UserDetailsServiceImpl(val userRepository: UserRepository) : UserDetailsService {
 
-override fun loadUserByUsername(username: String?): UserDetails {
-        TODO("Not yet implemented")
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException(username)
+        val grantedAuthorities = HashSet<GrantedAuthority>()
+        grantedAuthorities.add(SimpleGrantedAuthority(user.role))
+        return User(user.username, user.password, grantedAuthorities)
     }
 }
