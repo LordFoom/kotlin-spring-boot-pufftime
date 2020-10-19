7692c0c1b46eaa9d69b248520706c796afb13994
lordfoom <timothys@soliditech.com>
2020-10-19 20:20:54 +0200

    Removes unneeded data.sql file We can override the insert with the application properties file

--- src/test/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT into users(id, email, password, role, username, created)
-VALUES (1, 'lordfoom@gmail.com', '$2a$10$hs8j6FFDffDvBSqw44EBdOaiB2VhTaAIKAvAVmva.LKITcSjtPkBG', 'ADMIN', 'foom', now());
-
