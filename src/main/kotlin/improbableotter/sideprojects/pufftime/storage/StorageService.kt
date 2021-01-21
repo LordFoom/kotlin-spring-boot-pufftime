1f961612f6fe044c1632fe2f04edeb0fa1092f34
lordfoom <timothys@soliditech.com>
2021-01-21 21:17:30 +0200

    Last touches that complete file uploading Now to display!

--- src/main/kotlin/improbableotter/sideprojects/pufftime/storage/StorageService.kt
+++ src/main/kotlin/improbableotter/sideprojects/pufftime/storage/StorageService.kt
@@ -1,37 +1,34 @@
 package improbableotter.sideprojects.pufftime.storage
 
-import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.springframework.util.StringUtils
 import org.springframework.web.multipart.MultipartFile
+import java.io.File
 import java.io.IOException
-import java.lang.RuntimeException
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
-import java.util.stream.Stream
 
 
 @Service
 class StorageService {
 
-    val uploadDir ="./uploads/"
-    fun init(){
+    val uploadDir = "./uploads/"
+    fun init() {
 
     }
 
     /**
      * @return filepath where the file is stored
      */
-    fun store(file: MultipartFile):String{
+    fun store(file: MultipartFile): String {
         val fileName = file.originalFilename?.let { StringUtils.cleanPath(it) }
-
-        try{
-            val path = Paths.get(uploadDir+fileName)
+        File(uploadDir).mkdir()
+        try {
+            val path = Paths.get(uploadDir + fileName)
             Files.copy(file.inputStream, path, StandardCopyOption.REPLACE_EXISTING)
             return path.toString()
-        }catch( ioe: IOException){
+        } catch (ioe: IOException) {
             ioe.printStackTrace()
             throw RuntimeException(ioe)
         }
@@ -49,7 +46,7 @@ class StorageService {
 //
 //    }
 
-    fun deleteAll(){
+    fun deleteAll() {
 
     }
 
