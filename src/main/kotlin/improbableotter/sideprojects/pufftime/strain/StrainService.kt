bf5f77826889c8ce42092b1007393860632b222e
lordfoom <timothys@soliditech.com>
2021-05-31 20:01:55 +0200

    Initialize with args instead of @autowired

--- src/main/kotlin/improbableotter/sideprojects/pufftime/strain/StrainService.kt
+++ src/main/kotlin/improbableotter/sideprojects/pufftime/strain/StrainService.kt
@@ -1,16 +1,11 @@
 package improbableotter.sideprojects.pufftime.strain
 
 import improbableotter.sideprojects.pufftime.user.UserRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class StrainService {
-    @Autowired
-    lateinit var strainRepository: StrainRepository
-    @Autowired
-    lateinit var userRepository: UserRepository
+class StrainService(val strainRepository: StrainRepository, val userRepository: UserRepository) {
     fun create(dto: StrainDto): Strain {
         val user = userRepository.findByUsername(dto.username)
         user ?: throw IllegalStateException("No such user: " + dto.username)
@@ -18,16 +13,16 @@ class StrainService {
 
         val strain = strainRepository.findByName(dto.name)
 
-        strain?.let{
-            throw IllegalStateException("Strain already exists: "+dto.name)
+        strain?.let {
+            throw IllegalStateException("Strain already exists: " + dto.name)
         }
 
         return strainRepository.save(Strain.fromDto(dto))
     }
 
-    fun updateStrain(dto: StrainDto):Strain {
+    fun updateStrain(dto: StrainDto): Strain {
         strainRepository.findByIdOrNull(dto.id!!)
-                ?:throw IllegalStateException("No such strain: "+dto.name)
+            ?: throw IllegalStateException("No such strain: " + dto.name)
         return strainRepository.save(Strain.fromDto(dto))
     }
 }
